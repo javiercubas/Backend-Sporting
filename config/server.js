cf76a732bf75125464e4a36bfd52dd08495ba36a
javiercubas <javiercubas2003@gmail.com>
2022-09-13 13:51:58 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://sportingclubmadrid.com/api',
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
